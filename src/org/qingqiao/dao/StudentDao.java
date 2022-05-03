27dec8b8c1274eed5f0fbbe1a3c81488950326a3
Dahuang <anton3216@163.com>
2022-05-03 09:43:54 +0800

    查询所有数据

--- src/org/qingqiao/dao/StudentDao.java
+++ src/org/qingqiao/dao/StudentDao.java
@@ -1,7 +1,14 @@
 package org.qingqiao.dao;
 
+import org.qingqiao.bean.Clazz;
 import org.qingqiao.bean.Student;
+import org.qingqiao.utils.JDBCUtil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 // 数据控制 负责和数据库进行交互(增删改查)
@@ -9,7 +16,25 @@ public class StudentDao {
 
     // 查询所有数据
     public List<Student> queryAll(){
-        // 使用工具类获取连接
-        return null;
+        ArrayList<Student> list = new ArrayList<>();
+        try {
+            // 使用工具类获取连接
+            Connection conn = JDBCUtil.getConn();
+            // 编写sql语句
+            String sql = "select s.*,c.name cname from student s left join clazz c on s.cid = c.id";
+            // 创建预编译对象
+            PreparedStatement ps = conn.prepareStatement(sql);
+            // 执行sql语句
+            ResultSet rs = ps.executeQuery();
+            // 处理结果集
+            while(rs.next()){
+                Clazz clazz = new Clazz(rs.getInt(7),rs.getString(8));
+                Student student = new Student(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getInt(4),rs.getString(5),rs.getDouble(6),clazz);
+                list.add(student);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return list;
     }
 }
--- src/org/qingqiao/main/JDBCMain.java
+++ src/org/qingqiao/main/JDBCMain.java
@@ -1,8 +1,18 @@
 package org.qingqiao.main;
 
+import org.qingqiao.bean.Student;
+import org.qingqiao.dao.StudentDao;
+
+import java.util.List;
+
 public class JDBCMain {
 
     public static void main(String[] args) {
-
+        // 实例化StudentDao
+        StudentDao studentDao = new StudentDao();
+        List<Student> students = studentDao.queryAll();
+        for (Student student : students) {
+            System.out.println(student);
+        }
     }
 }
