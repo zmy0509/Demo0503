56d756bf29ec74dbe9b370e481ffedbc7480c1a5
Dahuang <anton3216@163.com>
2022-05-03 09:56:24 +0800

    查询所有班级 向学生表中进行添加

--- src/org/qingqiao/dao/StudentDao.java
+++ src/org/qingqiao/dao/StudentDao.java
@@ -14,7 +14,7 @@ import java.util.List;
 // 数据控制 负责和数据库进行交互(增删改查)
 public class StudentDao {
 
-    // 查询所有数据
+    // 查询所有学生数据
     public List<Student> queryAll(){
         ArrayList<Student> list = new ArrayList<>();
         try {
@@ -36,5 +36,49 @@ public class StudentDao {
             throwables.printStackTrace();
         }
         return list;
+    }
+
+    // 查询所有班级数据
+    public List<Clazz> queryAllClazz(){
+        ArrayList<Clazz> list = new ArrayList<>();
+        try {
+            // 使用工具类获取连接
+            Connection conn = JDBCUtil.getConn();
+            // 编写sql语句
+            String sql = "select * from clazz";
+            // 创建预编译对象
+            PreparedStatement ps = conn.prepareStatement(sql);
+            // 执行sql语句
+            ResultSet rs = ps.executeQuery();
+            // 处理结果集
+            while(rs.next()){
+                Clazz clazz = new Clazz(rs.getInt(1),rs.getString(2));
+                list.add(clazz);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return list;
+    }
+
+    // 添加数据
+    public int insertStudent(Student s){
+        int i = 0;
+        try {
+            Connection conn = JDBCUtil.getConn();
+            String sql = "insert into student(name,sex,age,address,score,cid) values(?,?,?,?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1,s.getName());
+            ps.setString(2,s.getSex());
+            ps.setInt(3,s.getAge());
+            ps.setString(4,s.getAddress());
+            ps.setDouble(5,s.getScore());
+            ps.setInt(6,s.getClazz().getId());
+
+            i = ps.executeUpdate();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return i;
     }
 }
--- src/org/qingqiao/main/JDBCMain.java
+++ src/org/qingqiao/main/JDBCMain.java
@@ -1,18 +1,49 @@
 package org.qingqiao.main;
 
+import org.qingqiao.bean.Clazz;
 import org.qingqiao.bean.Student;
 import org.qingqiao.dao.StudentDao;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class JDBCMain {
+    // 实例化StudentDao
+    static StudentDao studentDao = new StudentDao();
 
     public static void main(String[] args) {
-        // 实例化StudentDao
-        StudentDao studentDao = new StudentDao();
+        // insert();
+        queryall();
+    }
+
+    private static void queryall(){
         List<Student> students = studentDao.queryAll();
         for (Student student : students) {
             System.out.println(student);
+        }
+    }
+
+    private static void insert(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入姓名");
+        String name = sc.next();
+        System.out.println("请输入性别");
+        String sex = sc.next();
+        System.out.println("请输入年龄");
+        int age = sc.nextInt();
+        System.out.println("请输入地址");
+        String address = sc.next();
+        System.out.println("请输入成绩");
+        double score = sc.nextDouble();
+        // 调用查询所有班级信息
+        List<Clazz> clazzes = studentDao.queryAllClazz();
+        System.out.println(clazzes);
+        System.out.println("请输入班级");
+        int cid = sc.nextInt();
+        Student student = new Student(0, name, sex, age, address, score, new Clazz(cid, "只要是个字符串就行"));
+        int i = studentDao.insertStudent(student);
+        if(i > 0){
+            System.out.println("添加成功");
         }
     }
 }
